f057610aa0acc948dd45c386ec55a0fb3b546324
Richard Ouma <oumarichard505@gmail.com>
2026-03-30 14:04:57 +0300

    setting up the environment

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#FAFAFA] text-[#111111]">
+      <h1 className="p-10 text-4xl font-semibold">Achie D Closet</h1>
+    </div>
+  )
+}
+
+export default App
